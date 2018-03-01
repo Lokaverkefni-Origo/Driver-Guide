5f6c483b2dd945086a199959e475866b6be26ed2
etrausta <eythortra15@ru.is>
2018-03-01 10:53:21 +0000

    dialogflow komið inn og hægt að tala við hann og fá (smalltalk) response frá honum

--- app/src/main/java/com/etrausta/driverguide/MainChat.java
+++ app/src/main/java/com/etrausta/driverguide/MainChat.java
@@ -4,7 +4,9 @@ package com.etrausta.driverguide;
  * Created by jonth on 14.2.2018.
  */
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.support.design.widget.FloatingActionButton;
@@ -26,10 +28,26 @@ import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainChat extends Fragment {
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainChat extends Fragment implements AIListener {
     FloatingActionButton fab;
+    ListView listOfMsg;
+    EditText inputEditText;
+    private AIService aiService;
+    DatabaseReference ref;
+
     private FirebaseListAdapter<ChatMessage> adapter;
 
     public static MainChat newInstance() {
@@ -39,8 +57,10 @@ public class MainChat extends Fragment {
 
     @Override
     public void onCreate (Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -51,17 +71,64 @@ public class MainChat extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
+        final ai.api.android.AIConfiguration config = new ai.api.android.AIConfiguration("a9be4307cca64af69a4630bc6a6943af",
+                ai.api.android.AIConfiguration.SupportedLanguages.English,
+                ai.api.android.AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(getActivity(), config);
+        aiService.setListener(this);
+
+        final AIDataService aiDataService = new AIDataService(config);
+
+        final AIRequest aiRequest = new AIRequest();
+        ref = FirebaseDatabase.getInstance().getReference();
+        ref.keepSynced(true);
+
         fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("StaticFieldLeak")
             @Override
             public void onClick(View view) {
-                final EditText inputEditText = (EditText) getActivity().findViewById(R.id.inputEditText);
-                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(inputEditText.getText().toString(),
-                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-                inputEditText.setText("");
-                //FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage("þetta er dummy",
-                        //"Api Jónsson"));
+                inputEditText = (EditText) getActivity().findViewById(R.id.inputEditText);
+                final String message = inputEditText.getText().toString().trim();
 
+                if (!message.equals("")) {
+
+                    ChatMessage chatMessage = new ChatMessage(message, FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+                    ref.child("chat").push().setValue(chatMessage);
+
+                    aiRequest.setQuery(message);
+                    new AsyncTask<AIRequest, Void, AIResponse>() {
+                        @Override
+                        protected AIResponse doInBackground(AIRequest... aiRequests) {
+                            final AIRequest request = aiRequests[0];
+                            try {
+                                final AIResponse response = aiDataService.request(aiRequest);
+                                return response;
+                            } catch (AIServiceException e) {
+
+                            }
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(AIResponse response) {
+                            if (response != null) {
+                                Result result = response.getResult();
+                                String reply = result.getFulfillment().getSpeech();
+                                ChatMessage chatMessage = new ChatMessage(reply, "Caren");
+                                ref.child("chat").push().setValue(chatMessage);
+                            }
+                        }
+                    }.execute(aiRequest);
+                }
+                else {
+                    aiService.startListening();
+                }
+
+                inputEditText.setText("");
+;
                 //remove keyboard after clicking send
                 InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
@@ -73,11 +140,11 @@ public class MainChat extends Fragment {
     }
 
     private void displayChatMessage(View view) {
-        ListView listOfMsg = (ListView) view.findViewById(R.id.listOfMsg);
+        listOfMsg = (ListView) view.findViewById(R.id.listOfMsg);
         adapter = new FirebaseListAdapter<ChatMessage>(getActivity(),
                 ChatMessage.class,
                 R.layout.list_item,
-                FirebaseDatabase.getInstance().getReference()) {
+                ref.child("chat")) {
             @Override
             protected void populateView(View v, ChatMessage model, int position) {
                 //get references to the views of list_item.xml
@@ -92,5 +159,43 @@ public class MainChat extends Fragment {
             }
         };
         listOfMsg.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResult(AIResponse response) {
+        Result result = response.getResult();
+
+        String message = result.getResolvedQuery();
+        ChatMessage chatMessage0 = new ChatMessage(message, FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        ref.child("chat").push().setValue(chatMessage0);
+
+        String reply = result.getFulfillment().getSpeech();
+        ChatMessage chatMessage = new ChatMessage(reply, "Caren");
+        ref.child("chat").push().setValue(chatMessage);
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
